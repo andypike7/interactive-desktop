59070593a7f4bb4395acbbf527683118e8a6d89e
andypike7 <tech@sym.ru>
2021-03-07 11:50:53 +0300

    Parent, Centering

--- src/views/Section.vue
+++ src/views/Section.vue
@@ -5,16 +5,17 @@
       <vue-draggable-resizable
         v-for="(block, index) in blocks"
         :key="index"
-        :width="block.width"
-        :height="block.height"
         :minWidth="config.MIN_BLOCK_WIDTH"
         :minHeight="config.MIN_BLOCK_HEIGHT"
         :grid="[config.GRID, config.GRID]"
         :x="block.x"
         :y="block.y"
         :z="block.z"
+        :w="block.width"
+        :h="block.height"
         class="block"
         :style="{ background: block.background }"
+        parent
         @activated="blockActivated(index)"
         @dragging="blockDragged"
         @resizing="blockResized"
@@ -53,16 +54,16 @@ export default class Section extends Vue {
   blocks: Block[] = [];
   removedBlocks: Block[] = [];
   nextIndex = 0;
-  activeBlock = 0;
+  activeIndex = 0;
   config = CONFIG;
 
-  addBlock(): void {
+  addBlock(centered = false): void {
     this.blocks.push({
       name: `I'm an element #${++this.nextIndex}.`,
       width: CONFIG.DEFAULT_BLOCK_WIDTH,
       height: CONFIG.DEFAULT_BLOCK_HEIGHT,
-      x: Math.trunc(Math.random() * 30) * CONFIG.GRID,
-      y: Math.trunc(Math.random() * 30) * CONFIG.GRID,
+      x: centered ? this.centeredX() : this.randomX(),
+      y: centered ? this.centeredY() : this.randomY(),
       z: this.blocks.length,
       background:
         CONFIG.SET_OF_BACKGROUNDS[
@@ -71,56 +72,63 @@ export default class Section extends Vue {
     });
   }
 
+  randomX() {
+    return (
+      Math.trunc(
+        (Math.random() * (CONFIG.BOARD_WIDTH - CONFIG.DEFAULT_BLOCK_WIDTH)) /
+          CONFIG.GRID
+      ) * CONFIG.GRID
+    );
+  }
+  randomY() {
+    return (
+      Math.trunc(
+        (Math.random() * (CONFIG.BOARD_HEIGHT - CONFIG.DEFAULT_BLOCK_HEIGHT)) /
+          CONFIG.GRID
+      ) * CONFIG.GRID
+    );
+  }
+
   removeBlock(index: number): void {
     const removedBlocks = this.blocks.splice(index, 1);
 
     this.removedBlocks.push(removedBlocks[0]);
   }
 
+  centeredX() {
+    return (CONFIG.BOARD_WIDTH - CONFIG.DEFAULT_BLOCK_WIDTH) / 2;
+  }
+  centeredY() {
+    return (CONFIG.BOARD_HEIGHT - CONFIG.DEFAULT_BLOCK_HEIGHT) / 2;
+  }
+
   restoreBlock(): void {
     const restoredBlock = this.removedBlocks.pop();
 
     if (restoredBlock !== undefined) {
-      restoredBlock.x = (CONFIG.BOARD_WIDTH - CONFIG.DEFAULT_BLOCK_WIDTH) / 2;
-      restoredBlock.y = (CONFIG.BOARD_HEIGHT - CONFIG.DEFAULT_BLOCK_HEIGHT) / 2;
+      restoredBlock.x = this.centeredX();
+      restoredBlock.y = this.centeredY();
       restoredBlock.width = CONFIG.DEFAULT_BLOCK_WIDTH;
       restoredBlock.height = CONFIG.DEFAULT_BLOCK_HEIGHT;
       this.blocks.push(restoredBlock);
     }
   }
 
-  blockActivated(index: number): void {
-    this.activeBlock = index;
-    // console.log("*** CLICK!", index);
+  blockActivated(activeIndex: number): void {
+    this.activeIndex = activeIndex;
 
-    console.log(
-      "*** IN:",
-      this.blocks.map(block => {
-        return block.z;
-      })
-    );
-    console.log("*** index:", index);
-    const z = this.blocks[this.activeBlock].z;
-    console.log("*** Z:", z);
-
-    this.blocks.forEach((block, index2) => {
-      if (index === index2) {
+    const z = this.blocks[this.activeIndex].z;
+    this.blocks.forEach((block, index) => {
+      if (activeIndex === index) {
         block.z = this.nextIndex - 1;
       } else if (block.z > z) {
         block.z--;
       }
     });
-
-    console.log(
-      "*** OUT:",
-      this.blocks.map(block => {
-        return block.z;
-      })
-    );
   }
 
   blockResized(x: number, y: number, width: number, height: number): void {
-    const block = this.blocks[this.activeBlock];
+    const block = this.blocks[this.activeIndex];
 
     block.x = x;
     block.y = y;
@@ -129,7 +137,7 @@ export default class Section extends Vue {
   }
 
   blockDragged(x: number, y: number): void {
-    const block = this.blocks[this.activeBlock];
+    const block = this.blocks[this.activeIndex];
 
     block.x = x;
     block.y = y;
